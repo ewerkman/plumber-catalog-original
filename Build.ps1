1d373a911e59accb462fec8c22e404f9d8704f51
Erwin Werkman <werkman.erwin@gmail.com>
2018-09-23 10:00:54 +0200

    Updated CI

--- Build.ps1
+++ Build.ps1
@@ -2,4 +2,4 @@ Write-Host "Building:"
 Write-Host $Env:APPVEYOR_BUILD_VERSION
 
 dotnet build .\src\Plugin.Plumber.Catalog\Plugin.Plumber.Catalog.csproj
-dotnet pack .\src\Plugin.Plumber.Catalog\Plugin.Plumber.Catalog.csproj -c Release -o ..\..\artifacts
+dotnet pack .\src\Plugin.Plumber.Catalog\Plugin.Plumber.Catalog.csproj -c Release -o ..\..\artifacts /p:Version=$Env:APPVEYOR_BUILD_VERSION
